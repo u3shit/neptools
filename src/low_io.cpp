9c5ad286967c36b28895bae6b8a3fcabf3d35082
u3shit <u3shit@gmail.com>
2016-03-18 01:19:43 +0100

    fix compilation error in last commit

--- src/low_io.cpp
+++ src/low_io.cpp
@@ -19,9 +19,9 @@
 #include <iostream>
 
 #define SYSERROR2(x, rst)                                           \
-    NEPTOOLS_THROW(SystemError{std::error_code{                     \
+    NEPTOOLS_THROW((SystemError{std::error_code{                    \
                 int(GetLastError()), std::system_category()}} <<    \
-        boost::errinfo_api_function{x} rst)
+        boost::errinfo_api_function{x} rst))
 #define SYSERROR(x) SYSERROR2(x, )
 
 namespace Neptools
@@ -127,9 +127,9 @@ void LowIo::Write(const void* buf, FileMemSize len) const
 
 #else // linux/unix
 
-#define SYSERROR(x)                                                            \
-    NEPTOOLS_THROW(SystemError{std::error_code{errno, std::system_category()}} \
-                   << boost::errinfo_api_function{x})
+#define SYSERROR(x)                                                             \
+    NEPTOOLS_THROW((SystemError{std::error_code{errno, std::system_category()}} \
+                    << boost::errinfo_api_function{x}))
 
 namespace Neptools
 {
