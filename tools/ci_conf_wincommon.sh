384019dff159c6c987535084a36bd4125e6bf6ed
u3shit <u3shit@gmail.com>
2018-12-08 20:24:12 +0100

    libshit update

--- tools/ci_conf_wincommon.sh
+++ tools/ci_conf_wincommon.sh
@@ -6,6 +6,6 @@ run export HOST_LINKFLAGS=
 config_opts=(--clang-hack --lua-dll)
 
 tests=(
-    'wine build/stcm-editor.exe --xml-output=test-wine.xml --test -fc --reporters=junit,console'
+    'wine build/stcm-editor.exe --ansi-colors --xml-output=test-wine.xml --test -fc --reporters=junit,console'
     'tools/vmrun.sh /opt/win7-vm/vm.opts test-win7.xml build/stcm-editor.exe build/lua53.dll'
 )
